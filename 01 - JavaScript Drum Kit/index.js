2f1aef7f262b3b3631e724c729b38c44f74466da
Funsho Ayobanjo <funshoayobanjo@gmail.com>
2022-09-12 18:24:37 +0100

    drumkit complete

--- 01 - JavaScript Drum Kit/index.js
+++ 01 - JavaScript Drum Kit/index.js
@@ -1,24 +1,18 @@
-{
-  /* <script>
-function removeTransition(e) {
-  if (e.propertyName !== "transform") return;
-  e.target.classList.remove("playing");
-}
-
 function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-  if (!audio) return;
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+  if (!audio) return; // stops the function from running if data key is not present e.g no data key for key b, wont run
+  audio.currentTime = 0; //rewinds to the start for multiple selection or keydown
+  audio.play();
 
   key.classList.add("playing");
-  audio.currentTime = 0;
-  audio.play();
 }
 
-const keys = Array.from(document.querySelectorAll(".key"));
-keys.forEach((key) =>
-  key.addEventListener("transitionend", removeTransition)
-);
-window.addEventListener("keydown", playSound);
-</script> */
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return; // skip if its not a transform
+  this.classList.remove("playing");
 }
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+window.addEventListener("keydown", playSound);
